84fccb04adafcce6ea7a067cb9ece22bf571fc9b
Sergey Malyuga <sergey@malyuga.ru>
2026-02-04 16:39:24 +0300

    Создать компоненты основных страниц приложения.

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [],
+  templateUrl: './login.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginComponent {
+
+}
---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+
+}
