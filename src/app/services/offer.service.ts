ec216baee3e2f0eb3e788ee58709b826cdcbf91d
Sergey Malyuga <sergey@malyuga.ru>
2026-02-05 15:18:57 +0300

    Создать offer.service

---
+++ src/app/services/offer.service.ts
@@ -0,0 +1,24 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Offer, OfferPreview} from '../core/models/offers';
+import {Observable} from 'rxjs';
+import {AppRoute, BASE_URL} from '../core/constants/const';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OfferService {
+  private http = inject(HttpClient);
+
+  public getOffers(): Observable<OfferPreview[]> {
+    return this.http.get<OfferPreview[]>(`${BASE_URL}/${AppRoute.OFFER}`);
+  }
+
+  public getOfferById(offerId: string): Observable<Offer> {
+    return this.http.get<Offer>(`${BASE_URL}/${AppRoute.OFFER}/${offerId}`);
+  }
+
+  public getNearbyOffers(offerId: string): Observable<OfferPreview[]> {
+    return this.http.get<OfferPreview[]>(`${BASE_URL}/${AppRoute.OFFER}/${offerId}/nearby`);
+  }
+}
