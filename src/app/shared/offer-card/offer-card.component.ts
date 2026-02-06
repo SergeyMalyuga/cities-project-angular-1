348a3ae126f3ba67a655dd5faafcc74f7e9d5875
Sergey Malyuga <sergey@malyuga.ru>
2026-02-06 15:26:34 +0300

    Создать capitilize.pipe

--- src/app/shared/offer-card/offer-card.component.ts
+++ src/app/shared/offer-card/offer-card.component.ts
@@ -1,16 +1,17 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {OfferPreview} from '../../core/models/offers';
-import {of} from 'rxjs';
+import {CapitalizePipe} from '../pipes/capitilize.pipe';
 
 @Component({
   selector: 'app-offer-card',
-  imports: [],
+  imports: [
+    CapitalizePipe
+  ],
   templateUrl: './offer-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OfferCardComponent {
   @Input({required: true}) offer!: OfferPreview;
 
-  protected readonly Math = Math;
-  protected readonly of = of;
+  public readonly Math = Math;
 }
