0f72322a0893102a8f312d7504888f4b04b9cd55
Sergey Malyuga <sergey@malyuga.ru>
2026-02-19 15:55:40 +0300

    feat(login.component): создать loginGroup для login__form
    - loginGroup - реактивная форма с полями email и password
    - валидация полей

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,21 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {AppRoute} from '../../core/constants/const';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
-  protected readonly AppRoute = AppRoute;
+  private formBuilder = inject(FormBuilder);
+
+  public readonly AppRoute = AppRoute;
+
+  public loginGroup: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$')]],
+  })
 }
