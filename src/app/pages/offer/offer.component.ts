22162d0abd382a06b13c5d7602be587e87fd4bc9
Sergey Malyuga <sergey@malyuga.ru>
2026-02-26 15:32:07 +0300

    feat(offer.component): реализовать загрузку оффера
    - загрузка оффера осуществляется с помощью offerService
    - id требуемого оффера извлекается из activatedRoute.paramMap

--- src/app/pages/offer/offer.component.ts
+++ src/app/pages/offer/offer.component.ts
@@ -1,5 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { HeaderComponent } from '../../shared/components/header/header.component';
+import {ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit, signal} from '@angular/core';
+import {HeaderComponent} from '../../shared/components/header/header.component';
+import {OfferService} from '../../core/services/offer.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Offer} from '../../core/models/offers';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {catchError, EMPTY, forkJoin, switchMap} from 'rxjs';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../core/models/app.state';
 
 @Component({
   selector: 'app-offer-page',
@@ -7,4 +14,35 @@ import { HeaderComponent } from '../../shared/components/header/header.component
   templateUrl: './offer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OfferComponent {}
+export class OfferComponent implements OnInit {
+  private offerService = inject(OfferService);
+  private activatedRoute = inject(ActivatedRoute);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
+  private store = inject(Store<AppState>);
+
+  public offer = signal<Offer | undefined>(undefined);
+  public offerId = signal<string | null>(null);
+
+  public ngOnInit(): void {
+    this.activatedRoute.paramMap.pipe(
+      switchMap(params => {
+        const id = params.get('id');
+        if (id === null) {
+          this.router.navigate(['/', '**']);
+          return EMPTY;
+        }
+        this.offerId.set(id);
+        return forkJoin({
+          offer: this.offerService.getOfferById(id),
+        }).pipe(
+          catchError(() => {
+            this.router.navigate(['/', '**']);
+            return EMPTY;
+          })
+        )
+      }), takeUntilDestroyed(this.destroyRef)).subscribe(result =>
+      this.offer.set(result.offer)
+    );
+  }
+}
