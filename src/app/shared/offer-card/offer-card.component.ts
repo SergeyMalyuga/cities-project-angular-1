e99fdff7838473ec657b4d672af2f8d0891f4348
Sergey Malyuga <sergey@malyuga.ru>
2026-02-26 14:35:27 +0300

    feat(offer-card.component): реализовать переход на страницу выбранного оффера при клике на карточку оффера

--- src/app/shared/offer-card/offer-card.component.ts
+++ src/app/shared/offer-card/offer-card.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { OfferPreview } from '../../core/models/offers';
 import { CapitalizePipe } from '../pipes/capitilize.pipe';
+import {RouterLink} from '@angular/router';
+import {AppRoute} from '../../core/constants/const';
 
 @Component({
   selector: 'app-offer-card',
-  imports: [CapitalizePipe],
+  imports: [CapitalizePipe, RouterLink],
   templateUrl: './offer-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -12,4 +14,5 @@ export class OfferCardComponent {
   @Input({ required: true }) offer!: OfferPreview;
 
   public readonly Math = Math;
+  protected readonly AppRoute = AppRoute;
 }
