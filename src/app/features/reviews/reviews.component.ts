a3b603d3236dd6ade5005e09b9d724c0698b57dd
Sergey Malyuga <sergey@malyuga.ru>
2026-03-02 14:14:34 +0300

    feat(reviews-form.component): создать компонент
    - подключить к reviews.component

--- src/app/features/reviews/reviews.component.ts
+++ src/app/features/reviews/reviews.component.ts
@@ -1,12 +1,14 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {ReviewsListComponent} from '../reviews-list/reviews-list.component';
 import {Comment} from '../../core/models/comments';
+import {ReviewsFormComponent} from '../reviews-form/reviews-form.component';
 
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
   imports: [
-    ReviewsListComponent
+    ReviewsListComponent,
+    ReviewsFormComponent
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
