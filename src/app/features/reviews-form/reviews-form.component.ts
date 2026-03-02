e9e8eb1d0a71cccb651feac65bff323da93adb68
Sergey Malyuga <sergey@malyuga.ru>
2026-03-02 14:27:50 +0300

    feat(reviews-form.component): реализовать валидацию
    - создать reviewGroup с помощью FormBuilder
    - валидация rating: required
    - валидация comment: required, minLength(50), maxLength(356)
    - если reviewGroup не валидна отключить кнопку form__submit button

--- src/app/features/reviews-form/reviews-form.component.ts
+++ src/app/features/reviews-form/reviews-form.component.ts
@@ -1,11 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reviews-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './reviews-form.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ReviewsFormComponent {
+  private formBuilder = inject(FormBuilder);
 
+  public reviewGroup = this.formBuilder.group({
+    rating: ['', [Validators.required]],
+    comment: ['', [Validators.required, Validators.required, Validators.minLength(50), Validators.maxLength(356)]],
+  })
+
+  public onSubmit() {
+    console.log(this.reviewGroup.valid);
+  }
 }
