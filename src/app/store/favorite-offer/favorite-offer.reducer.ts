ffd9996d75af4dfa1d854e87a677d3f2d32018d8
Sergey Malyuga <sergey@malyuga.ru>
2026-03-06 13:49:59 +0300

    feat(favorite-offer.reducer): добавить обработку favorite-offer.actions
    - loadFavoriteOffers -> установка isLoading: true
    - loadFavoriteOffersSuccess -> обновление состояния в store, сбрасывает error и isLoading
    - loadFavoriteOffersFailure -> сохраняет error, сбрасывает isLoading

--- src/app/store/favorite-offer/favorite-offer.reducer.ts
+++ src/app/store/favorite-offer/favorite-offer.reducer.ts
@@ -1,7 +1,12 @@
-import { createEntityAdapter } from '@ngrx/entity';
-import { OfferPreview } from '../../core/models/offers';
-import { FavoriteOffersState } from '../../core/models/favorite-offers.state';
-import { createReducer } from '@ngrx/store';
+import {createEntityAdapter} from '@ngrx/entity';
+import {OfferPreview} from '../../core/models/offers';
+import {FavoriteOffersState} from '../../core/models/favorite-offers.state';
+import {createReducer, on} from '@ngrx/store';
+import {
+  loadFavoriteOffers,
+  loadFavoriteOffersFailure,
+  loadFavoriteOffersSuccess
+} from './actions/favorite-offer.actions';
 
 export const favoriteOfferAdapter = createEntityAdapter<OfferPreview>();
 const initialState: FavoriteOffersState = favoriteOfferAdapter.getInitialState({
@@ -9,4 +14,12 @@ const initialState: FavoriteOffersState = favoriteOfferAdapter.getInitialState({
   error: null,
 });
 
-export const favoriteOffersReducer = createReducer(initialState);
+export const favoriteOffersReducer = createReducer(initialState,
+  on(loadFavoriteOffers, state => ({
+    ...state, isLoading: true
+  })),
+  on(loadFavoriteOffersSuccess, (state, {favoriteOffers}) =>
+    favoriteOfferAdapter.setAll(favoriteOffers, {...state, isLoading: false, error: null})),
+  on(loadFavoriteOffersFailure, (state, {error}) => ({
+    ...state, isLoading: false, error
+  })));
