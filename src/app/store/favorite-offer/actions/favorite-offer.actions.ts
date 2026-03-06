f7c92dcd06ee7d674816eb2c33852f2763d01071
Sergey Malyuga <sergey@malyuga.ru>
2026-03-06 13:23:16 +0300

    feat(favorite-offer.actions): создать actions избранных офферов
    - loadFavoriteOffers - инициализация загрузки
    - loadFavoriteOffersSuccess - успешная загрузка
    - loadFavoriteOffersFailure - ошибка при загрузке

---
+++ src/app/store/favorite-offer/actions/favorite-offer.actions.ts
@@ -0,0 +1,8 @@
+import {createAction, props} from '@ngrx/store';
+import {OfferPreview} from '../../../core/models/offers';
+
+export const loadFavoriteOffers = createAction('[App Component] Load Favorite Offers');
+export const loadFavoriteOffersSuccess = createAction('Favorite Offer API] Load Favorite Offer Success',
+  props<{favoriteOffers: OfferPreview[]}>());
+export const loadFavoriteOffersFailure = createAction('Favorite Offer API] Load Favorite Offer Failure',
+  props<{error: string}>());
